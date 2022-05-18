d55490dbcf8e9f61ccc8fa2b86603b39112f13b3
onyx6838 <giang.mink@gmail.com>
2022-05-18 23:52:08 +0700

    excel

--- src/pages/dashboards/Report/index.js
+++ src/pages/dashboards/Report/index.js
@@ -10,7 +10,7 @@ const Report = () => {
                     <Card.Header>
                         <Card.Title>Các báo cáo</Card.Title>
                         <h6 className="card-subtitle text-muted">
-                            Giúp cho tra cứu thông tin hoặc lưu trữ lại dễ dàng
+                            Tải xuống để tra cứu, xem thông tin và lưu trữ
                         </h6>
                     </Card.Header>
                     <Table>
@@ -22,7 +22,7 @@ const Report = () => {
                         </thead>
                         <tbody>
                             <tr>
-                                <td>Báo cáo quản lý sản phẩm</td>
+                                <td>Quản lý sản phẩm</td>
                                 <td className="table-action">
                                     <a href='http://localhost:8080/api/v1/admin/baocao/sanphams/export/excel'
                                         target='_blank' download rel="noopener noreferrer">
@@ -31,9 +31,18 @@ const Report = () => {
                                 </td>
                             </tr>
                             <tr>
-                                <td>Báo cáo lượng sản phẩm bán ra theo từng tháng trong năm nay ({new Date().getFullYear()})</td>
+                                <td>Lượng sản phẩm bán ra theo từng tháng trong năm nay ({new Date().getFullYear()})</td>
                                 <td className="table-action">
                                     <a href={`http://localhost:8080/api/v1/admin/baocao/sp-ban-thang-nam/export/excel/${new Date().getFullYear()}`}
+                                        target='_blank' download rel="noopener noreferrer">
+                                        <Icon.Download className="align-middle me-1" size={18}></Icon.Download>
+                                    </a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Doanh thu tổng hợp</td>
+                                <td className="table-action">
+                                    <a href={`http://localhost:8080/api/v1/admin/baocao/bcao-tong-hop/export/excel/${new Date().getFullYear()}`}
                                         target='_blank' download rel="noopener noreferrer">
                                         <Icon.Download className="align-middle me-1" size={18}></Icon.Download>
                                     </a>
